c0c1abecf5fe8fed254d0f28fa261cee5d6e3af6
NEFORCEO <pirogartem919@gmail.com>
2025-11-14 13:20:43 +0700

    UPDATE COMMIT

--- database/models/register.py
+++ database/models/register.py
@@ -1,3 +1,4 @@
+from client.config.config import DataBase
 from database.database.db import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer
@@ -5,7 +6,7 @@ from sqlalchemy import String, Integer
 
 class Register(Base):
     
-    __tablename__ = "users"
+    __tablename__ = DataBase.table_name
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, index=True) 
     password: Mapped[str] = mapped_column(String, index=True)
